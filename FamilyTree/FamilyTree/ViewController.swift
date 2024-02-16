5320af781e02522d7da2879785fdc1f64890e90b
samdav3 <sdavenport@students.mchenry.edu>
2024-02-15 18:40:15 -0600

    initial commit

---
+++ FamilyTree/FamilyTree/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FamilyTree
+//
+//  Created by Sam Davenport on 2/15/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
