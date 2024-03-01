2c426e0e8479153a849507b10a9d6f59a253fc38
samdav3 <sdavenport@students.mchenry.edu>
2024-02-29 19:21:36 -0600

    committ

---
+++ MyTableViewApp/MyTableViewApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyTableViewApp
+//
+//  Created by Sam Davenport on 2/29/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
